0a4e82762e0f761ef5adc2604d76c04db623b60a
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-27 14:55:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,25 @@
-require 'pry'
-
 class Song
   attr_accessor :name, :artist
   @@all = []
 
   def initialize(name)
     @name = name
-    @artist = artist
     @@all << self
+  end
+
+  def artist_name
+    if @artist
+      @artist.name
+    else
+      return nil
+    end
   end
 
   def self.all
     @@all
   end
 
-#not passing, returns object not artist name
-  def artist_name
-    self.artist
+  def name
+    @name
   end
-
-# need to add conditional -
-# if artist name is included in artist all,
-#   then return artist name
-# else return nil
-
-
-  def song_count
-    @@all.length
-  end
-
 end
